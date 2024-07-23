3243e36db0dabdb9f7959e2dda9c8261f50e6488
아기새 <liebespaar93@gmail.com>
2024-07-23 23:11:16 +0900

    Unreal Engine (Ver 5.4.3) Default Project & MainPR Defence

---
+++ Source/Tekken8.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Tekken8Target : TargetRules
+{
+	public Tekken8Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("Tekken8");
+	}
+}
---
+++ Source/Tekken8/Tekken8.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tekken8.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tekken8, "Tekken8" );
