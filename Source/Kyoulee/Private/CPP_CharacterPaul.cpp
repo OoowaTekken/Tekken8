5b274d2687c01842bc97326b51508f7e09a296c3
아기새 <liebespaar93@gmail.com>
2024-07-26 18:01:47 +0900

    Add Move Command And animation

--- Source/Kyoulee/Private/CPP_CharacterPaul.cpp
+++ Source/Kyoulee/Private/CPP_CharacterPaul.cpp
@@ -9,13 +9,13 @@ ACPP_CharacterPaul::ACPP_CharacterPaul()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
 	USkeletalMeshComponent* tempCharacterMesh = this->GetMesh();
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempSkeletalMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"));
 	if (tempSkeletalMesh.Succeeded())
 		tempCharacterMesh->SetSkeletalMeshAsset(tempSkeletalMesh.Object);
 	tempCharacterMesh->SetRelativeLocation(FVector(0,0,-90));
 	tempCharacterMesh->SetRelativeRotation(FRotator(0,0,-90));
+
 }
 
 int32 ACPP_CharacterPaul::GetCurrInputKey()
@@ -69,16 +69,20 @@ int32 ACPP_CharacterPaul::ShowInputKey(int ArrowKey, bool LeftArm, bool RightArm
 	return int32();
 }
 
-
 void ACPP_CharacterPaul::SettingCommandTree()
 {
-	//this->mBaseCommandTree.Add(ShowInputKey(1, false, false, false, false), CreatCommandTree(1,1,1, ));
-
-	this->mBaseCommandTree[ShowInputKey(1, false, false, false, false)];
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(4, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command1);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(6, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command2);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(4, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command3);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(6, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command4);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(4, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command6);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(6, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command7);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(4, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command8);
+	this->AddCommandTree(mBaseCommandTree, ShowInputKey(6, 0, 0, 0, 0), 4, 1, 2, &ACPP_CharacterPaul::Command9);
 }
 
 
-FCommandTree* ACPP_CharacterPaul::CreatCommandTree(int32 timingStart, int32 timingEnd, int32 timingAction, void (* fptr)())
+FCommandTree* ACPP_CharacterPaul::CreateCommandTree(int32 timingStart, int32 timingEnd, int32 timingAction, void (ACPP_CharacterPaul::* fptr)())
 {
 	FCommandTree *NewCommand = new FCommandTree();
 	
@@ -87,30 +91,33 @@ FCommandTree* ACPP_CharacterPaul::CreatCommandTree(int32 timingStart, int32 timi
 	NewCommand->timingStart = timingStart;
 	NewCommand->timingEnd = timingEnd;
 	NewCommand->timingAction = timingAction;
-	NewCommand->ActionFunction = fptr;
-	NewCommand->ActionFunction();
+	NewCommand->action.BindUObject(this, fptr);
+
+	//NewCommand->action.Execute();
 	return NewCommand;
+}
+
+FCommandTree* ACPP_CharacterPaul::AddCommandTree(TMap<int32, FCommandTree*> &CurrCommandTree,int32 keyValue, int32 timingStart, int32 timingEnd, int32 timingAction, void(ACPP_CharacterPaul::* fptr)())
+{
+//  음 빈거 확인해야 하는데 잘안됨 확인할 필요있음
+// 	if (!CurrCommandTree && nullptr == CurrCommandTree[keyValue])
+// 	{
+// 		UE_LOG(LogTemp, Warning, TEXT("Error: befor add CommandTree was settinged!! %i"), keyValue);
+// 		return nullptr;
+// 	}
+
+	CurrCommandTree.Add(keyValue, this->CreateCommandTree(timingStart,timingEnd, timingAction, fptr));
+	return CurrCommandTree[keyValue];
 }
 
 // Called when the game starts or when spawned
 void ACPP_CharacterPaul::BeginPlay()
 {
 	Super::BeginPlay();
-}
 
-// Called every frame
-void ACPP_CharacterPaul::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-	
-	// 주의 모든 작업은 FrameSystem에서 하는걸 권장합니다.
-	
-	if (this->fFrameTime < this->fCurrTimeForFrame)
-	{
-		this->fCurrTimeForFrame = 0;
-		this->FrameSystem();
-	}
-	
+	// 커멘드를 설정합니다.
+	this->mBaseCommandTree.Add(1,CreateCommandTree(1,1,1,&ACPP_CharacterPaul::Command4));
+	this->SettingCommandTree();
 }
 
 // Called to bind functionality to input
@@ -119,8 +126,96 @@ void ACPP_CharacterPaul::SetupPlayerInputComponent(UInputComponent* PlayerInputC
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
+
+// Called every frame
+void ACPP_CharacterPaul::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// 주의 모든 작업은 FrameSystem에서 하는걸 권장합니다.
+	this->fCurrTimeForFrame += DeltaTime;
+
+	if (this->fCurrTimeForFrame > this->fFrameTime)
+	{
+		this->fCurrTimeForFrame = 0;
+		this->FrameSystem();
+	}
+	
+}
+/**
+ * Frame 시스템을 위한 함수입니다.
+ */
 void ACPP_CharacterPaul::FrameSystem()
 {
-	
+	 FVector dir = this->ToLocation - this->GetActorLocation() / 60;
+	 this->SetActorLocation(this->GetActorLocation() + dir);
+	if (!(fCurrFrame % 60))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("test frame"));
+	}
+	if (fCurrFrame > 60)
+		fCurrFrame = 0;
+	fCurrFrame++;
+}
+
+
+void ACPP_CharacterPaul::SetToLocationPoint(float x, float y, float z)
+{
+	FVector moveToPoint = this->GetActorLocation() +
+	(
+		this->GetActorForwardVector() * x +
+		this->GetActorRightVector() * y +
+		this->GetActorUpVector() * z
+	);
+	this->ToLocation = moveToPoint;
+}
+
+void ACPP_CharacterPaul::Command1()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command1 Pressed"));
+	this->SetToLocationPoint(-5, 0, 0);
+}
+
+void ACPP_CharacterPaul::Command2()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command2 Pressed"));
+	this->SetToLocationPoint(0, 0, 0);
+}
+
+void ACPP_CharacterPaul::Command3()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command3 Pressed"));
+	this->SetToLocationPoint(5, 0, 0);
+}
+
+void ACPP_CharacterPaul::Command4()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command4 Pressed"));
+	this->SetToLocationPoint(-10, 0, 0);
+}
+
+void ACPP_CharacterPaul::Command6()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command6 Pressed"));
+	this->SetToLocationPoint(10, 0, 0);
+}
+
+void ACPP_CharacterPaul::Command7()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command7 Pressed"));
+	this->SetToLocationPoint(-10, 0, 10);
+}
+
+void ACPP_CharacterPaul::Command8()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command8 Pressed"));
+	this->SetToLocationPoint(0, 0, 10);
+}
+
+void ACPP_CharacterPaul::Command9()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Command9 Pressed"));
+	this->SetToLocationPoint(10, 0, 10);
+
 }
 
--- Source/Kyoulee/Public/CPP_CharacterPaul.h
+++ Source/Kyoulee/Public/CPP_CharacterPaul.h
@@ -7,6 +7,10 @@
 #include "CPP_CharacterPaul.generated.h"
 
 
+// 델리게이트
+DECLARE_DELEGATE(FActoin);
+
+
 USTRUCT(Atomic, BlueprintType)
 struct FInputKey
 {
@@ -58,9 +62,11 @@ public:
 	int32 timingAction;
 	
 	// 발동 함수
-	void (* ActionFunction)();
+	FActoin action;
  };
  
+
+
 
 UCLASS()
 class KYOULEE_API ACPP_CharacterPaul : public ACharacter
@@ -70,7 +76,6 @@ class KYOULEE_API ACPP_CharacterPaul : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ACPP_CharacterPaul();
-
 
 	/**
 	 * @title 기본적인 키에대한 정보입니다.
@@ -95,10 +100,14 @@ public:
 
 	void SettingCommandTree();
 
-	FCommandTree *CreatCommandTree(int32 timingStart, int32 timingEnd, int32 timingAction, void (*fptr)());
+	FCommandTree *CreateCommandTree(int32 timingStart, int32 timingEnd, int32 timingAction, void (ACPP_CharacterPaul::*fptr)());
 	
-	FCommandTree *AddCommand(int32 keyValue);
-	void skillfinctiontest(){};
+	FCommandTree *AddCommandTree(TMap<int32, FCommandTree*>& CurrCommandTree, int32 keyValue, int32 timingStart, int32 timingEnd, int32 timingAction, void(ACPP_CharacterPaul::* fptr)());
+
+
+
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -116,15 +125,30 @@ public:
 	 */
 	void FrameSystem();
 
-	float fFrameTime = 1 / 60;
-	float fCurrTimeForFrame = 0;
+	float fFrameTime = (1 / 60)  * 1000 ;
+	float fCurrTimeForFrame = 60;
+	int32 fCurrFrame = 0;
 
-
+	void skillfinctiontest() {};
+	
+	/**
+	 * @title Next Location Data 처리
+	 * @brief 애니메이션을 위한 작업입니다.
+	 */
+	FVector ToLocation;
+	void SetToLocationPoint(float x, float y, float z);
 	/**
 	* @title 메인트리
 	* @brief 시작 지점 트리를 가지고 있습니다.
 	*	모든 스킬트리는 맵형식의 반복된 값을 가지고 있음으로 참고 바랍니다.
  	*/
-	//TMap<int32, FCommandTree> mMainCommandTree;
+	void Command1();
+	void Command2();
+	void Command3();
+	void Command4();
+	void Command6();
+	void Command7();
+	void Command8();
+	void Command9();
 };
 	            
